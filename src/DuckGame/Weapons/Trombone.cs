9d4f8944f4eb9d8183ceef625cee6f168acf0a6f
The Fool <theflyingfoool@gmail.com>
2022-08-14 20:38:35 -0500

    Trombone hand fixed
    graphics bug

--- src/DuckGame/Weapons/Trombone.cs
+++ src/DuckGame/Weapons/Trombone.cs
@@ -1,158 +1,181 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: DuckGame.Trombone
-//removed for regex reasons Culture=neutral, PublicKeyToken=null
-// MVID: C907F20B-C12B-4773-9B1E-25290117C0E4
-// Assembly location: D:\Program Files (x86)\Steam\steamapps\common\Duck Game\DuckGame.exe
-// XML documentation location: D:\Program Files (x86)\Steam\steamapps\common\Duck Game\DuckGame.xml
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace DuckGame
 {
-    [EditorGroup("Guns|Misc")]
-    [BaggedProperty("isFatal", false)]
-    public class Trombone : Gun
-    {
-        public StateBinding _notePitchBinding = new StateBinding(nameof(notePitch));
-        public StateBinding _handPitchBinding = new StateBinding(nameof(handPitch));
-        public float notePitch;
-        public float handPitch;
-        private float prevNotePitch;
-        private float hitPitch;
-        private Sound noteSound;
-        private List<InstrumentNote> _notes = new List<InstrumentNote>();
-        private Sprite _slide;
-        private float _slideVal;
+	[EditorGroup("Guns|Misc")]
+	[BaggedProperty("isFatal", false)]
+	public class Trombone : Gun
+	{
+		public Trombone(float xval, float yval) : base(xval, yval)
+		{
+			this.ammo = 4;
+			this._ammoType = new ATLaser();
+			this._ammoType.range = 170f;
+			this._ammoType.accuracy = 0.8f;
+			this.wideBarrel = true;
+			this.barrelInsertOffset = new Vec2(-1f, -3f);
+			this._type = "gun";
+			this.graphic = new Sprite("tromboneBody", 0f, 0f);
+			this.center = new Vec2(10f, 16f);
+			this.collisionOffset = new Vec2(-4f, -5f);
+			this.collisionSize = new Vec2(8f, 11f);
+			this._barrelOffsetTL = new Vec2(19f, 14f);
+			this._fireSound = "smg";
+			this._fullAuto = true;
+			this._fireWait = 1f;
+			this._kickForce = 3f;
+			this._holdOffset = new Vec2(6f, 2f);
+			this._slide = new Sprite("tromboneSlide", 0f, 0f);
+			this._slide.CenterOrigin();
+			this._notePitchBinding.skipLerp = true;
+			this.editorTooltip = "Just look at this thing. It's amazing. The instrument of kings.";
+			this.isFatal = false;
+		}
 
-        public Trombone(float xval, float yval)
-          : base(xval, yval)
-        {
-            ammo = 4;
-            _ammoType = new ATLaser();
-            _ammoType.range = 170f;
-            _ammoType.accuracy = 0.8f;
-            wideBarrel = true;
-            barrelInsertOffset = new Vec2(-1f, -3f);
-            _type = "gun";
-            graphic = new Sprite("tromboneBody");
-            center = new Vec2(10f, 16f);
-            collisionOffset = new Vec2(-4f, -5f);
-            collisionSize = new Vec2(8f, 11f);
-            _barrelOffsetTL = new Vec2(19f, 14f);
-            _fireSound = "smg";
-            _fullAuto = true;
-            _fireWait = 1f;
-            _kickForce = 3f;
-            _holdOffset = new Vec2(6f, 2f);
-            _slide = new Sprite("tromboneSlide");
-            _slide.CenterOrigin();
-            _notePitchBinding.skipLerp = true;
-            editorTooltip = "Just look at this thing. It's amazing. The instrument of kings.";
-            isFatal = false;
-        }
+		public override void Initialize()
+		{
+			base.Initialize();
+		}
 
-        public override void Initialize() => base.Initialize();
+		public float NormalizePitch(float val)
+		{
+			return val;
+		}
 
-        public float NormalizePitch(float val) => val;
+		public override void Update()
+		{
+			Duck d = this.owner as Duck;
+			if (d != null)
+			{
+				if (base.isServerForObject && d.inputProfile != null)
+				{
+					this.handPitch = d.inputProfile.leftTrigger;
+					if (d.inputProfile.hasMotionAxis)
+					{
+						this.handPitch += d.inputProfile.motionAxis;
+					}
+					int keyboardNote = Keyboard.CurrentNote(d.inputProfile, this);
+					if (keyboardNote >= 0)
+					{
+						this.notePitch = (float)keyboardNote / 12f + 0.01f;
+						this.handPitch = this.notePitch;
+						if (this.notePitch != this.prevNotePitch)
+						{
+							this.prevNotePitch = 0f;
+							if (this.noteSound != null)
+							{
+								this.noteSound.Stop();
+								this.noteSound = null;
+							}
+						}
+					}
+					else if (d.inputProfile.Down("SHOOT"))
+					{
+						this.notePitch = this.handPitch + 0.01f;
+					}
+					else
+					{
+						this.notePitch = 0f;
+					}
+				}
+				if (this.notePitch != this.prevNotePitch)
+				{
+					if (this.notePitch != 0f)
+					{
+						int note = (int)Math.Round((double)(this.notePitch * 12f));
+						if (note < 0)
+						{
+							note = 0;
+						}
+						if (note > 12)
+						{
+							note = 12;
+						}
+						if (this.noteSound == null)
+						{
+							this.hitPitch = this.notePitch;
+							Sound snd = SFX.Play("trombone" + Change.ToString(note), 1f, 0f, 0f, false);
+							this.noteSound = snd;
+							Level.Add(new MusicNote(base.barrelPosition.x, base.barrelPosition.y, base.barrelVector));
+						}
+						else
+						{
+							this.noteSound.Pitch = Maths.Clamp(this.notePitch - this.hitPitch, -1f, 1f);
+						}
+					}
+					else if (this.noteSound != null)
+					{
+						this.noteSound.Stop();
+						this.noteSound = null;
+					}
+				}
+				if (this._raised)
+				{
+					this.handAngle = 0f;
+					this.handOffset = new Vec2(0f, 0f);
+					this._holdOffset = new Vec2(0f, 2f);
+					this.collisionOffset = new Vec2(-4f, -7f);
+					this.collisionSize = new Vec2(8f, 16f);
+				}
+				else
+				{
+					this.handOffset = new Vec2(6f + (1f - this.handPitch) * 4f, -4f + (1f - this.handPitch) * 4f);
+					this.handAngle = (1f - this.handPitch) * 0.4f * (float)this.offDir;
+					this._holdOffset = new Vec2(5f + this.handPitch * 2f, -9f + this.handPitch * 2f);
+					this.collisionOffset = new Vec2(-4f, -7f);
+					this.collisionSize = new Vec2(2f, 16f);
+					this._slideVal = 1f - this.handPitch;
+				}
+			}
+			else
+			{
+				this.collisionOffset = new Vec2(-4f, -5f);
+				this.collisionSize = new Vec2(8f, 11f);
+			}
+			this.prevNotePitch = this.notePitch;
+			base.Update();
+		}
 
-        public override void Update()
-        {
-            if (this.owner is Duck owner)
-            {
-                if (isServerForObject && owner.inputProfile != null)
-                {
-                    handPitch = owner.inputProfile.leftTrigger;
-                    if (owner.inputProfile.hasMotionAxis)
-                        handPitch += owner.inputProfile.motionAxis;
-                    int num = Keyboard.CurrentNote(owner.inputProfile, this);
-                    if (num >= 0)
-                    {
-                        notePitch = (float)(num / 12.0 +  0.01f);
-                        handPitch = notePitch;
-                        if (notePitch != prevNotePitch)
-                        {
-                            prevNotePitch = 0f;
-                            if (noteSound != null)
-                            {
-                                noteSound.Stop();
-                                noteSound = null;
-                            }
-                        }
-                    }
-                    else
-                        notePitch = !owner.inputProfile.Down("SHOOT") ? 0f : handPitch + 0.01f;
-                }
-                if (notePitch != prevNotePitch)
-                {
-                    if (notePitch != 0.0)
-                    {
-                        int num = (int)Math.Round(notePitch * 12.0);
-                        if (num < 0)
-                            num = 0;
-                        if (num > 12)
-                            num = 12;
-                        if (noteSound == null)
-                        {
-                            hitPitch = notePitch;
-                            noteSound = SFX.Play("trombone" + Change.ToString(num));
-                            Level.Add(new MusicNote(barrelPosition.x, barrelPosition.y, barrelVector));
-                        }
-                        else
-                            noteSound.Pitch = Maths.Clamp(notePitch - hitPitch, -1f, 1f);
-                    }
-                    else if (noteSound != null)
-                    {
-                        noteSound.Stop();
-                        noteSound = null;
-                    }
-                }
-                if (_raised)
-                {
-                    handAngle = 0f;
-                    handOffset = new Vec2(0f, 0f);
-                    _holdOffset = new Vec2(0f, 2f);
-                    collisionOffset = new Vec2(-4f, -7f);
-                    collisionSize = new Vec2(8f, 16f);
-                }
-                else
-                {
-                    handOffset = new Vec2((float)(6.0 + (1.0 - handPitch) * 4.0), (float)((1.0 - handPitch) * 4.0 - 4.0));
-                    handAngle = (float)((1.0 - handPitch) * 0.04f) * offDir;
-                    _holdOffset = new Vec2((float)(5.0 + handPitch * 2.0), (float)(handPitch * 2.0 - 9.0));
-                    collisionOffset = new Vec2(-4f, -7f);
-                    collisionSize = new Vec2(2f, 16f);
-                    _slideVal = 1f - handPitch;
-                }
-            }
-            else
-            {
-                collisionOffset = new Vec2(-4f, -5f);
-                collisionSize = new Vec2(8f, 11f);
-            }
-            prevNotePitch = notePitch;
-            base.Update();
-        }
+		public override void OnPressAction()
+		{
+		}
 
-        public override void OnPressAction()
-        {
-        }
+		public override void OnReleaseAction()
+		{
+		}
 
-        public override void OnReleaseAction()
-        {
-        }
+		public override void Fire()
+		{
+		}
 
-        public override void Fire()
-        {
-        }
+		public override void Draw()
+		{
+			base.Draw();
+			Material material = Graphics.material;
+			Graphics.material = base.material;
+			base.Draw(this._slide, new Vec2(6f + this._slideVal * 8f, 0f), -1);
+			Graphics.material = material;
+		}
 
-        public override void Draw()
-        {
-            base.Draw();
-            Material material = Graphics.material;
-            Graphics.material = this.material;
-            Draw(_slide, new Vec2((float)(6.0 + _slideVal * 8.0), 0f), -1);
-            Graphics.material = material;
-        }
-    }
+		public StateBinding _notePitchBinding = new StateBinding("notePitch", -1, false, false);
+
+		public StateBinding _handPitchBinding = new StateBinding("handPitch", -1, false, false);
+
+		public float notePitch;
+
+		public float handPitch;
+
+		private float prevNotePitch;
+
+		private float hitPitch;
+
+		private Sound noteSound;
+
+		private List<InstrumentNote> _notes = new List<InstrumentNote>();
+
+		private Sprite _slide;
+
+		private float _slideVal;
+	}
 }
