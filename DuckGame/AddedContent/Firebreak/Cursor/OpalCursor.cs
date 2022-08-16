f5f3654de678bfe3169cc937819e1010e08744f2
Firebreak-2 <69664260+Firebreak-2@users.noreply.github.com>
2022-08-16 21:58:21 +0400

    Added some prep for custom cursor support
    Added the GameCursor.cs abstract class to create cursors with. This will then be handled by CursorHandler.cs to deal with drawing or updating the cursor.
    Added a WIP OpalCursor.cs which will be a mouse mainly used for debugging. Currently does nothing-ish.

---
+++ DuckGame/AddedContent/Firebreak/Cursor/OpalCursor.cs
@@ -0,0 +1,28 @@
+﻿using DuckGame.AddedContent.Drake.PolyRender;
+
+namespace DuckGame;
+
+public class OpalCursor : GameCursor
+{
+    public static bool DotFollow = false;
+    
+    public override string Id => "opal";
+    public override void DrawCursor()
+    {
+        PolyRenderer.Line(Mouse.position.ButX(-2, true), Mouse.position.ButX(2, true), 0.5f, Color.White);
+        PolyRenderer.Line(Mouse.position.ButY(-2, true), Mouse.position.ButY(2, true), 0.5f, Color.White);
+        
+        if (DotFollow)
+            PolyRenderer.Circle(Mouse.position, 4f, 24, Color.Red);
+    }
+
+    public override void LeftPressed(Vec2 position)
+    {
+        DotFollow = true;
+    }
+
+    public override void LeftReleased(Vec2 position)
+    {
+        DotFollow = false;
+    }
+}
