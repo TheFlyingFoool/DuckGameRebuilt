2336c11dddc109fbb1a4a154fa8ee96525f6834a
The Fool <theflyingfoool@gmail.com>
2022-09-18 00:29:06 -0500

    Update DuckGame.sh

--- deps/DuckGame.sh
+++ deps/DuckGame.sh
@@ -5,4 +5,4 @@ log_file=/home/$USER/Desktop/log.txt
 function log(){
 echo "$@" | tee -a "${log_file}"
 }
-mono /home/dan/Desktop/bin5/DuckGame.exe -nodinput -nothreading | tee -a "${log_file}"
+mono ./DuckGame.exe -nodinput -nothreading | tee -a "${log_file}"
