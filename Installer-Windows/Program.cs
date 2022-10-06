13f48d14dc8d6fae1a0df3f4a97439074b692594
klof44 <klof4four@gmail.com>
2022-10-05 19:52:34 -0500

    Added experimental installer for Windows

---
+++ Installer-Windows/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Installer_Windows
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new RebuiltInstaller());
+        }
+    }
+}
