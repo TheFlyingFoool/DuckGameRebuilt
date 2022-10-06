94cb1d8a85ec4fb89af5d7c8ec5afb1d141d7c44
othello7 <othello71@outlook.com>
2022-10-05 22:58:10 -0400

    i386 libs and duckgame.sh

--- deps/DuckGame.sh
+++ deps/DuckGame.sh
@@ -6,4 +6,4 @@ rm -v $log_file
 function log(){
 echo "$@" | tee -a "${log_file}"
 }
-mono ./DuckGame.exe -nodinput -nothreading | tee -a "${log_file}"
+mono ./DuckGame.exe $1 | tee -a "${log_file}"
