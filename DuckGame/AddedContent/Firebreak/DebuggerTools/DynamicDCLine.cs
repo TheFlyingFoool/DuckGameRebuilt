1040af7bb5ceaeb3fef976f3264c7487f2bbfa3f
Firebreak <xxibrahimgamerxx123123@gmail.com>
2022-10-07 19:25:56 +0400

    no crash on startup this time !!11!!

---
+++ DuckGame/AddedContent/Firebreak/DebuggerTools/DynamicDCLine.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DuckGame
+{
+    public class DynamicDCLine
+    {
+        public int LineIndex;
+        public readonly Func<int, string?> GetLine;
+        private int _iteration;
+        
+        public DynamicDCLine(Func<int, string?> lineFunction)
+        {
+            GetLine = lineFunction;
+        }
+
+        public void Done()
+        {
+            s_allDynamicLines.Remove(this);
+        }
+
+        public void Log()
+        {
+            string line = GetLine(_iteration--);
+            
+            if (line is not null)
+            {
+                LineIndex = DevConsole.core.lines.Count;
+                DevConsole.Log(line);
+                s_allDynamicLines.Add(this);
+                return;
+            }
+            
+            Done();
+        }
+
+        private static List<DynamicDCLine> s_allDynamicLines = new();
+
+        [DrawingContext(CustomID = "dyn_dc_update")]
+        public static void UpdateLines()
+        {
+            for (int i = 0; i < s_allDynamicLines.Count; i++)
+            {
+                var item = s_allDynamicLines[i];
+
+                string line = item.GetLine(item._iteration++);
+
+                if (line is not null && item._iteration > 0)
+                    DevConsole.core.lines.ElementAt(item.LineIndex).line = line;
+                else if (item._iteration != 0)
+                    item.Done();
+            }
+        }
+    }
+}
--- src/MonoTime/Console/Commands/Debug.cs
+++ src/MonoTime/Console/Commands/Debug.cs
@@ -4,63 +4,12 @@ using System.Text.RegularExpressions;
 
 namespace DuckGame
 {
-
-
     public static partial class DevConsoleCommands
     {
-        private static int? _lineIndex = null;
-        private static ProgressValue _progress = new();
-        private static int _nextUpdateFrame = 0;
-
         [DevConsoleCommand(Description = "If this command still exists after release im gonna eat my shoes")]
-        public static object Debug(int i)
+        public static void Debug()
         {
-            switch (i)
-            {
-                case 0:
-                    {
-                        _nextUpdateFrame = 0;
-                        _progress = new ProgressValue(0, 0.03, 0, 20);
-                        // DevConsole.Log($"[{_progress.GenerateBar()}] {_progress.NormalizedValue}%");
-                        DevConsole.Log(_progress.NormalizedValue);
-                        _lineIndex = DevConsole.core.lines.Count;
-                        break;
-                    }
-                case 1:
-                    {
-                        ProgressValue p = new ProgressValue(35, 1, 0, 100);
-                        DevConsole.Log(p);
-                        break;
-                    }
-            }
-
-            return null;
-        }
-
-        [DrawingContext]
-        public static void DcTestLineUpdate()
-        {
-            if (_lineIndex is null || _lineIndex >= DevConsole.core.lines.Count || _lineIndex < 0 || _nextUpdateFrame++ >= 30)
-                return;
-
-            _progress++;
-            _progress = ~_progress;
-
-            string highlightColor = _progress.NormalizedValue switch
-            {
-                >= 1.0f => "|DGGREEN|",
-                >= 0.5f => "|DGYELLOW|",
-                _ => "|DGRED|",
-            };
-
-            DevConsole.core.lines.ElementAt(_lineIndex.Value).line =
-                $"{Regex.Replace($"[{_progress.GenerateBar()}]", @"#+", @"|DGGREEN|$&|WHITE|")}" +
-                $"{highlightColor} {Math.Round(_progress.NormalizedValue * 100, 2)}%";
-
-            _nextUpdateFrame = 0;
-
-            if (!_progress.Completed)
-                _lineIndex = null;
+            new DynamicDCLine(i => i < 1000 ? i.ToString() : null).Log(); 
         }
     }
 }
