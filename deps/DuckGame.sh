6a5c5f71da2ff8acb84b5c77b5870daf2fa09bb1
othello7 <othello71@outlook.com>
2023-01-01 19:49:50 -0500

    fixed dan's namespace and edited DuckGame.sh

--- deps/DuckGame.sh
+++ deps/DuckGame.sh
@@ -1,9 +1,2 @@
-#!/bin/bash
-
-log_file=/home/$USER/Desktop/log.txt
-rm -v $log_file
-
-function log(){
-echo "$@" | tee -a "${log_file}"
-}
-mono ./DuckGame.exe $1 | tee -a "${log_file}"
+#!/usr/bin/env bash
+mono ./DuckGame.exe $@ | tee outputlog.txt
