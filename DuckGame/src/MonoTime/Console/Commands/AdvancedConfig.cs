54a797dc85ec67302a1e4924dfc3883720e1dc57
Firebreak-2 <XxibrahimgamerxX123123@gmail.com>
2023-02-19 14:28:39 +0400

    Added `advancedconfig` command. Opens config folder

---
+++ DuckGame/src/MonoTime/Console/Commands/AdvancedConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace DuckGame
+{
+
+    public static partial class DevConsoleCommands
+    {
+        [DevConsoleCommand(Description = "Opens the AdvancedConfig folder",
+            Aliases = new[] {"advconf"})]
+        public static void AdvancedConfig()
+        {
+            Process.Start(AdvancedConfigAttribute.SaveDirPath);
+            DevConsole.Log("Opened AdvancedConfig folder");
+        }
+    }
+}
