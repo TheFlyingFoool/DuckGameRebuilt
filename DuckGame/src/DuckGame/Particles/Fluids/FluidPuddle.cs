066f73da2dfa0836f454d9638b7d0179f769b31f
NiK0 <44278422+NiKled@users.noreply.github.com>
2023-05-25 21:05:56 -0300

    Fixed issue with stuff floating on fluids when it shouldn't

--- DuckGame/src/DuckGame/Particles/Fluids/FluidPuddle.cs
+++ DuckGame/src/DuckGame/Particles/Fluids/FluidPuddle.cs
@@ -1,11 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: DuckGame.FluidPuddle
-//removed for regex reasons Culture=neutral, PublicKeyToken=null
-// MVID: C907F20B-C12B-4773-9B1E-25290117C0E4
-// Assembly location: D:\Program Files (x86)\Steam\steamapps\common\Duck Game\DuckGame.exe
-// XML documentation location: D:\Program Files (x86)\Steam\steamapps\common\Duck Game\DuckGame.xml
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace DuckGame
@@ -94,10 +87,8 @@ namespace DuckGame
 
         public override void Initialize()
         {
-            if (_leftCorner == null || _rightCorner == null)
-                Level.Remove(this);
-            else
-                y = _leftCorner.corner.y;
+            if (_leftCorner == null || _rightCorner == null) Level.Remove(this);
+            else y = _leftCorner.corner.y;
         }
 
         public void Feed(FluidData dat)
@@ -261,10 +252,7 @@ namespace DuckGame
                 Level.CheckRectAll(topLeft, bottomRight, _coll);
                 foreach (PhysicsObject physicsObject in _coll)
                 {
-                    if (physicsObject.buoyancy > 0f)
-                    {
-                        physicsObject.sleeping = false;
-                    }
+                    physicsObject.sleeping = false;
                 }
             }
             FluidPuddle fluidPuddle = Level.CheckLine<FluidPuddle>(new Vec2(left, y), new Vec2(right, y), this);
