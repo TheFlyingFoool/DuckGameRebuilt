45d6418c2b24cf8a2acfec742548809898b4b8b1
NiK0 <44278422+NiKled@users.noreply.github.com>
2023-07-27 17:35:18 -0300

    Fixed certain collision issues

--- DuckGame/src/DuckGame/Stuff/Ragdoll.cs
+++ DuckGame/src/DuckGame/Stuff/Ragdoll.cs
@@ -1,11 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: DuckGame.Ragdoll
-//removed for regex reasons Culture=neutral, PublicKeyToken=null
-// MVID: C907F20B-C12B-4773-9B1E-25290117C0E4
-// Assembly location: D:\Program Files (x86)\Steam\steamapps\common\Duck Game\DuckGame.exe
-// XML documentation location: D:\Program Files (x86)\Steam\steamapps\common\Duck Game\DuckGame.xml
-
-using System;
+﻿using System;
 
 namespace DuckGame
 {
@@ -421,7 +414,11 @@ namespace DuckGame
             _duck._jumpValid = 0;
             _duck._lastHoldItem = null;
             _makeActive = false;
-            _part2.ReturnItemToWorld(_duck);
+
+            //TODO: Re-optimize this code
+            //the collision code here was reverted because pyramid upclipping worked differently than in normal DG because of
+            //collision buckets or something -NiK0
+            _part2.OldReturnItemToWorld(_duck);
             if (Network.isActive)
             {
                 active = false;
