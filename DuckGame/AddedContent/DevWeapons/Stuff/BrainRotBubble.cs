c6385a201b413112cfa24b739e5e09bb830a99b6
klof44 <klof4four@gmail.com>
2023-09-14 11:36:30 -0500

    klofGun V0.1

---
+++ DuckGame/AddedContent/DevWeapons/Stuff/BrainRotBubble.cs
@@ -0,0 +1,48 @@
+﻿namespace DuckGame
+{
+    public class BrainRotBubble : Thing
+    {
+        public SpriteMap sprite;
+        RotTypes type;
+        private Duck target;
+        public BrainRotBubble(float xval, float yval, Duck target, RotTypes type = RotTypes.Amogus) : base(xval, yval)
+        { 
+            this.type = type;
+            this.target = target;
+
+            switch (type)
+            {
+                case RotTypes.Amogus:
+                    sprite = new SpriteMap("amogusbubble", 28, 28);
+                    break;
+            }
+            graphic = sprite;
+        }
+
+        public override void Update()
+        {
+            if (isServerForObject)
+            {
+                if (target.ragdoll != null)
+                {
+                    position = new Vec2(target.ragdoll.position + new Vec2(5, -34f));
+                }
+                else if (target._trappedInstance != null)
+                {
+                    position = new Vec2(target._trappedInstance.position + new Vec2(5, -34f));
+                }
+                else
+                {
+                    position = new Vec2(target.position + new Vec2(5, -34f));
+                }
+            }
+            base.Update();
+        }
+        
+        public enum RotTypes
+        {
+            Random = 0,
+            Amogus = 1,
+        }
+    }
+}
---
+++ DuckGame/AddedContent/DevWeapons/Stuff/BrainRotBullet.cs
@@ -0,0 +1,67 @@
+﻿namespace DuckGame
+{
+    public class BrainRotBullet : Thing, ITeleport
+    {
+        public SpriteMap orbSprite;
+        public SpriteMap ringSprite;
+        public BrainRotBullet(float xpos, float ypos, Vec2 travel) : base(xpos, ypos) {
+            _travel = travel;
+            _collisionSize = new Vec2(12f, 12f);
+            _collisionOffset = new Vec2(2f, 2f);
+            orbSprite = new SpriteMap("brainrotorb", 18, 18);
+            ringSprite = new SpriteMap("brainrotring", 18, 18);
+            ringSprite.center = new Vec2(9f, 9f);
+            orbSprite.center = new Vec2(9f, 9f);
+            graphic = orbSprite;
+            wobble = new aWobbleMaterial(this, 0.2f);
+        }
+
+        private Vec2 _travel;
+        public int safeFrames = 15;
+
+        public override void Update()
+        {
+            position += _travel * 1f;
+            if (isServerForObject && (x > Level.current.bottomRight.x + 200f || x < Level.current.topLeft.x - 200f))
+            {
+                Level.Remove(this);
+            }
+            foreach (Duck duck in Level.CheckRectAll<Duck>(topLeft, bottomRight))
+            {
+                if (safeFrames <= 0 && duck.isServerForObject)
+                {
+                    duck.GiveBrainRot();
+                }
+            }
+            if (safeFrames > 0)
+            {
+                safeFrames--;
+            }
+            base.Update();
+        }
+        
+        aWobbleMaterial wobble;
+        public override void Draw()
+        {
+            orbSprite.imageIndex = 0;
+            base.Draw();
+            orbSprite.imageIndex = 1;
+            Graphics.material = wobble;
+            depth -= 1;
+            base.Draw();
+            depth += 1;
+            Graphics.material = null;
+            
+            ringSprite.imageIndex = 0;
+            ringSprite.position = position + new Vec2(9f, 9f);
+            ringSprite.angle += 0.1f;
+            ringSprite.Draw();
+            ringSprite.depth -= 1;
+            ringSprite.imageIndex = 1;
+            Graphics.material = wobble;
+            ringSprite.Draw();
+            ringSprite.depth += 1;
+            Graphics.material = null;
+        }
+    }
+}
