3069174dc830423a205228f2ee6f55cfd1c63038
othello7 <othello71@outlook.com>
2023-09-14 18:15:33 -0400

    fine tuned the funny

--- DuckGame/AddedContent/DevWeapons/Stuff/DodgeblockCurse.cs
+++ DuckGame/AddedContent/DevWeapons/Stuff/DodgeblockCurse.cs
@@ -19,22 +19,34 @@ namespace DuckGame
         public Duck duck;
         public override void Draw()
         {
-            Graphics.DrawRect(topLeft - new Vec2(0, 288), bottomRight + new Vec2(0, 288), Color.White * 0.3f, -1);
+            Graphics.DrawRect(topLeft - new Vec2(0, 288), bottomRight + new Vec2(0, BlockH * BoardHeight), Color.Black * 0.4f, -1);
 
             for (int i = 0; i < falling.Count; i++)
             {
                 DodgeBlock block = falling[i];
-                Vec2 v = new Vec2(block.x, block.y) * 16;
+                Vec2 v = new Vec2(block.x * BlockW, block.y * BlockH);
 
-                Graphics.DrawString("%", position + v + new Vec2(1, 9), Color.Black, 1.1f, null ,2);
-                Graphics.DrawRect(position + v + new Vec2(0, 8), position + new Vec2(16, 24) + v, Color.White, 1);
+                if (block.special)
+                {
+                    Graphics.DrawString("^", position + v + new Vec2(0.4f, 8 + (BlockH / 2f) - 5.9f), Color.Black, 1.1f, null, 1.68f); //calculated alignment -othello7
+                    Graphics.DrawRect(position + v + new Vec2(0, 8), position + new Vec2(BlockW, BlockH + 8) + v, Color.Yellow, 1);
+                }
+                else
+                {
+                    Graphics.DrawString("%", position + v + new Vec2(2, 8 + (BlockH / 2f) - 5.9f), Color.Black, 1.1f, null, 1.68f); //calculated pixel-perfect scale alignment -othello7
+                    Graphics.DrawRect(position + v + new Vec2(0, 8), position + new Vec2(BlockW, BlockH + 8) + v, Color.White, 1);
+                }
             }
         }
+
+        private bool rewarded = false;
         public int timer;
-
         public BitBuffer bFall = new BitBuffer();
-
         public List<DodgeBlock> falling = new List<DodgeBlock>();
+        private const int BlockH = 24;
+        private const int BlockW = 16;
+        private const int BoardHeight = 18;
+
         public override void Update()
         {
             if (isServerForObject)
@@ -44,13 +56,14 @@ namespace DuckGame
                 {
                     DodgeBlock fall = falling[i];
                     if (inc) fall.y++;
-                    if (fall.y == 18)
+                    if (fall.y == BoardHeight)
                     {
                         falling.RemoveAt(i);
                     }
                 }
 
-                if (timer % 40 == 0 || (Rando.Int(3) == 0 && timer % 20 == 0)) falling.Add(new DodgeBlock() { x = Rando.Int(-3, 2), y = -18 });
+                if (timer == 1000) falling.Add(new DodgeBlock() { x = Rando.Int(-3, 2), y = -18, special = true });
+                else if (timer % 20 == 0) falling.Add(new DodgeBlock() { x = Rando.Int(-3, 2), y = -18, special = false });
 
                 if (duck != null)
                 {
@@ -117,17 +130,32 @@ namespace DuckGame
                 for (int i = 0; i < falling.Count; i++)
                 {
                     DodgeBlock block = falling[i];
-                    Vec2 v = new Vec2(block.x, block.y) * 16;
+                    Vec2 v = new Vec2(block.x * BlockW, block.y * BlockH);
 
-                    IEnumerable<IAmADuck> iaads = Level.CheckRectAll<IAmADuck>(position + v + new Vec2(0, 8), position + new Vec2(16, 24) + v);
+                    IEnumerable<IAmADuck> iaads = Level.CheckRectAll<IAmADuck>(position + v + new Vec2(0, (BlockH - 1) + 8), position + new Vec2(BlockW, BlockH + 8) + v);
                     foreach (IAmADuck iaad in iaads)
                     {
                         MaterialThing mt = (MaterialThing)iaad;
                         Duck d = Duck.GetAssociatedDuck(mt);
                         if (d != null && d == duck && !d.dead)
                         {
-                            d.MakeStars();
-                            d.Kill(new DTCrush(null));
+                            if (block.special)
+                            {
+                                if (!rewarded)
+                                    for (int j = 0; j < 3; j++)
+                                    {
+                                        Thing reward = ItemBoxRandom.GetRandomItem();
+                                        reward.position = d.position;
+                                        Level.Add(reward);
+                                    }
+                                rewarded = true;
+
+                            }
+                            else
+                            {
+                                d.MakeStars();
+                                d.Kill(new DTCrush(null));
+                            }
                         }
                     }
                 }
@@ -138,5 +166,6 @@ namespace DuckGame
     {
         public int x;
         public int y;
+        public bool special; //for funny ending powerup -othello7
     }
 }
