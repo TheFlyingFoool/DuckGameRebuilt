4997d6e4957051127e1d1a38ba24636c735c33b8
Firebreak-2 <XxibrahimgamerxX123123@gmail.com>
2023-10-07 17:16:21 +0400

    Add `args` command

---
+++ DuckGame/src/MonoTime/Console/Commands/LaunchArguments.cs
@@ -0,0 +1,14 @@
+﻿namespace DuckGame
+{
+
+    public static partial class DevConsoleCommands
+    {
+        [DevConsoleCommand(
+            Description = "Prints your currently active program launch arguments",
+            Aliases = new[] { "args", "commandline", "launchargs" })]
+        public static string LaunchArguments()
+        {
+            return Program.commandLine;
+        }
+    }
+}
