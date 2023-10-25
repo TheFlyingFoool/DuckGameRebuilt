f87546816f9772a5b27c39570f1b56c82b1e4a9f
Tater <Mr.Potatooh@gmail.com>
2023-10-25 16:50:21 -0500

    Remove decompilation comments

--- DuckGame/src/MonoTime/Sound/OggPlayer.cs
+++ DuckGame/src/MonoTime/Sound/OggPlayer.cs
@@ -1,11 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: DuckGame.OggPlayer
-//removed for regex reasons Culture=neutral, PublicKeyToken=null
-// MVID: C907F20B-C12B-4773-9B1E-25290117C0E4
-// Assembly location: D:\Program Files (x86)\Steam\steamapps\common\Duck Game\DuckGame.exe
-// XML documentation location: D:\Program Files (x86)\Steam\steamapps\common\Duck Game\DuckGame.xml
-
-using Microsoft.Xna.Framework.Audio;
+﻿using Microsoft.Xna.Framework.Audio;
 using NVorbis;
 using System;
 using System.Globalization;
