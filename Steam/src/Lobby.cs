68b9ec568542fde6b77d2921a1cac2167b71a67b
TheFlyingFoool <48270355+TheFlyingFoool@users.noreply.github.com>
2024-01-25 12:06:06 -0600

    added Steam.initialized to Lobby.cs To prevent crashing issues

--- Steam/src/Lobby.cs
+++ Steam/src/Lobby.cs
@@ -25,7 +25,7 @@ public class Lobby : IDisposable {
         get {
             try
             {
-                if (id != 0)
+                if (id != 0 && Steam.initialized)
                 {
                     return User.GetUser(SteamMatchmaking.GetLobbyOwner(_id));
                 }
@@ -40,20 +40,22 @@ public class Lobby : IDisposable {
     }
     public unsafe void SetLobbyModsData(string value)
     {
-        if (id != 0)
+        if (id != 0 && Steam.initialized)
         {
             SteamMatchmaking.SetLobbyData(_id, "mods", value);
         }
     }
     public unsafe string name {
-        get {
-            if (id == 0) {
-                return "";
+        get
+        {
+            if (id != 0 && Steam.initialized)
+            {
+                return SteamMatchmaking.GetLobbyData(_id, "name");
             }
-            return SteamMatchmaking.GetLobbyData(_id, "name");
+            return "";
         }
         set {
-            if (id != 0) {
+            if (id != 0 && Steam.initialized) {
                 SteamMatchmaking.SetLobbyData(_id, "name", value);
             }
         }
@@ -65,7 +67,7 @@ public class Lobby : IDisposable {
             return id != 0 && _joinable;
         }
         set {
-            if (id != 0) {
+            if (id != 0 && Steam.initialized) {
                 SteamMatchmaking.SetLobbyJoinable(_id, value);
                 _joinable = value;
             }
@@ -78,7 +80,7 @@ public class Lobby : IDisposable {
             return _type;
         }
         set {
-            if (id != 0) {
+            if (id != 0 && Steam.initialized) {
                 SteamMatchmaking.SetLobbyType(_id, (ELobbyType) value);
                 // This isn't set by the original Steam.dll...
                 _type = value;
@@ -88,29 +90,32 @@ public class Lobby : IDisposable {
 
     private int _maxMembers;
     public unsafe int maxMembers {
-        get {
-            if (id == 0) {
-                return 0;
+        get
+        {
+            if (id != 0 && Steam.initialized)
+            {
+                return _maxMembers = SteamMatchmaking.GetLobbyMemberLimit(_id);
             }
-            return _maxMembers = SteamMatchmaking.GetLobbyMemberLimit(_id);
+            return 0;
         }
         set {
-            if (id != 0) {
+            if (id != 0 && Steam.initialized) {
                 SteamMatchmaking.SetLobbyMemberLimit(_id, _maxMembers = value);
             }
         }
     }
 
     public unsafe List<User> users {
-        get {
-            if (id == 0) {
-                return new List<User>();
+        get
+        {
+            if (id != 0 && Steam.initialized)
+            {
+                return SteamHelper.GetList(SteamMatchmaking.GetNumLobbyMembers(_id), i => User.GetUser(SteamMatchmaking.GetLobbyMemberByIndex(_id, i)));
             }
 
-            return SteamHelper.GetList(SteamMatchmaking.GetNumLobbyMembers(_id), i => User.GetUser(SteamMatchmaking.GetLobbyMemberByIndex(_id, i)));
+            return new List<User>();
         }
     }
-
     public bool processing { get; private set; }
 
     public Lobby(ulong lobbyID)
@@ -148,14 +153,15 @@ public class Lobby : IDisposable {
     }
 
     public unsafe void SetLobbyData(string name, string value) {
-        if (id != 0)
+        if (id != 0 && Steam.initialized)
             SteamMatchmaking.SetLobbyData(_id, name, value);
     }
 
-    public unsafe string GetLobbyData(string name) {
-        if (id == 0)
-            return "";
-        return SteamMatchmaking.GetLobbyData(_id, name);
+    public unsafe string GetLobbyData(string name)
+    {
+        if (id != 0 && Steam.initialized)
+            return SteamMatchmaking.GetLobbyData(_id, name);
+        return "";
     }
 
     [HandleProcessCorruptedStateExceptions]
