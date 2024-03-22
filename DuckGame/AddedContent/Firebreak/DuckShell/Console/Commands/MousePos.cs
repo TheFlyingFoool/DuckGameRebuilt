2c8ababec9e45f691cfa01e862a68f4e519402b4
Firebreak-2 <XxibrahimgamerxX123123@gmail.com>
2024-03-22 14:39:33 +0400

    add MousePos DSH command

---
+++ DuckGame/AddedContent/Firebreak/DuckShell/Console/Commands/MousePos.cs
@@ -0,0 +1,27 @@
+﻿using AddedContent.Firebreak;
+using System;
+
+namespace DuckGame.ConsoleEngine
+{
+    public static partial class Commands
+    {
+        [Marker.DevConsoleCommand(Description = "Returns the current mouse position")]
+        public static Vec2 MousePos(MousePositionDimension dimension = default)
+        {
+            return dimension switch
+            {
+                MousePositionDimension.World   => Mouse.positionScreen,
+                MousePositionDimension.Screen  => Mouse.position,
+                MousePositionDimension.Console => Mouse.positionConsole,
+                _                              => throw new ArgumentOutOfRangeException(nameof(dimension), dimension, null)
+            };
+        }
+        
+        public enum MousePositionDimension
+        {
+            World,
+            Screen,
+            Console
+        }
+    }
+}
