8511624fd446673325a2182d230884f980ecd447
Dzhake <dzhakenir@gmail.com>
2024-03-22 12:16:37 +0300

    Add "MousePos" command

---
+++ DuckGame/AddedContent/Firebreak/DuckShell/Console/Commands/Mousepos.cs
@@ -0,0 +1,27 @@
+﻿using AddedContent.Firebreak;
+
+namespace DuckGame.ConsoleEngine
+{
+    public static partial class Commands
+    {
+        public enum MousePositionDimension
+        {
+            World,
+            Screen,
+            Console
+        }
+
+        [Marker.DevConsoleCommand(Description = "Returns mouse position", To = ImplementTo.DuckShell)]
+        public static string MousePos(MousePositionDimension dimension = default)
+        {
+            Vec2 position = dimension switch
+            {
+                MousePositionDimension.World => Mouse.positionScreen,
+                MousePositionDimension.Screen => Mouse.position,
+                MousePositionDimension.Console => Mouse.positionConsole
+            };
+
+            return $"{position.x},{position.y}";
+        }
+    }
+}
