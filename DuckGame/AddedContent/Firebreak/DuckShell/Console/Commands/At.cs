2722e36281e1bc5450b49c291e8b6826cbb5678e
Firebreak-2 <XxibrahimgamerxX123123@gmail.com>
2024-03-24 12:42:33 +0400

    Add `at` DSH command for collection indexing

---
+++ DuckGame/AddedContent/Firebreak/DuckShell/Console/Commands/At.cs
@@ -0,0 +1,13 @@
+﻿using AddedContent.Firebreak;
+
+namespace DuckGame.ConsoleEngine
+{
+    public static partial class Commands
+    {
+        [Marker.DevConsoleCommand(Description = "Gets the element in the collection at the specified index", To = ImplementTo.DuckShell)]
+        public static string At(int index, DSHKeyword @in, string[] collection)
+        {
+            return collection[index];
+        }
+    }
+}
