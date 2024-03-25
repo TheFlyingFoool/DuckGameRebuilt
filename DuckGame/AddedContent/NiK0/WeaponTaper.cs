2d5c1938bb09ecd46f631f5b0132dbf7aa059ae9
NiK0 <44278422+NiKled@users.noreply.github.com>
2024-03-25 11:08:27 -0300

    Rare event

--- DuckGame/AddedContent/NiK0/WeaponTaper.cs
+++ DuckGame/AddedContent/NiK0/WeaponTaper.cs
@@ -1,11 +1,4 @@
-﻿using NAudio.MediaFoundation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DuckGame
+﻿namespace DuckGame
 {
     [ClientOnly]
     [EditorGroup("Equipment")]
