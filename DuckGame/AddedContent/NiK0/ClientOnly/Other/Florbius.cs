cdaff443e0689f16e5a1b6dea046d0f4b690589e
NiK0 <44278422+NiKled@users.noreply.github.com>
2024-05-27 15:59:44 -0300

    Added Florbius

---
+++ DuckGame/AddedContent/NiK0/ClientOnly/Other/Florbius.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuckGame
+{
+    public class Florbius //All powerful. All knowing. Florbius. Bow down to your new god.
+    {
+    }
+}
