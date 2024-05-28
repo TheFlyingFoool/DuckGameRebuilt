2c8b266e40ad5d87e76c09dcdefb8280a0d53f55
Dzhake <andryzindmi@gmail.com>
2024-05-28 20:00:32 +0300

    Change "Scoring" match setting view

--- DuckGame/src/DuckGame/Network/DuckNetwork.cs
+++ DuckGame/src/DuckGame/Network/DuckNetwork.cs
@@ -1046,11 +1046,11 @@ namespace DuckGame
                 Main.SpecialCode = "men2";
                 _core._matchModifierMenu.SetBackFunction(new UIMenuActionOpenMenu(_core._matchModifierMenu, _core._matchSettingMenu));
                 _core._matchModifierMenu.Close();
-                _core._matchSettingMenu.Add(new UIMenuItemNumber("Scoring type", field: new FieldBinding(typeof(TeamSelect2), nameof(TeamSelect2.KillsForPoints), 0, 2, 1), valStrings: new List<string>()
+                _core._matchSettingMenu.Add(new UIMenuItemNumber("Scoring", field: new FieldBinding(typeof(TeamSelect2), nameof(TeamSelect2.KillsForPoints), 0, 2, 1), valStrings: new List<string>()
                 {
                     "Normal",
-                    "Kills ",
-                    "Both  ",
+                    "Kills",
+                    "Both",
                 }, c: Colors.DGPink));
                 _core._matchSettingMenu.AddMatchSetting(TeamSelect2.GetOnlineSetting("teams"), false);
                 //_core._matchSettingMenu.Add(new UISideButton(66, -50, 50, 0, "@SHOOT@"));
