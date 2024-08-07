f8824c99009877ed16e42c08077705226723ed22
NiK0 <44278422+NiKled@users.noreply.github.com>
2024-08-07 11:08:35 -0300

    Update Profile.cs

--- DuckGame/src/DuckGame/Profile/Profile.cs
+++ DuckGame/src/DuckGame/Profile/Profile.cs
@@ -676,7 +676,7 @@ namespace DuckGame
                 if (muteName)
                     nameUi = $"Player {networkIndex + 1}";
                 
-                if (isUsingRebuilt)
+                if (isUsingRebuilt && !DGRSettings.HSDClearNames)
                 {
                     string dgrMojiName = "DGRBIG";
 
